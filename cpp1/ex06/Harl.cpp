27835c7950f90b55ed2d58debf91d6617151db60
lefoffan - leon <lefoffan@student.42perpignan.fr>
2025-08-06 13:31:36 +0200

    understood how switch work, ex06 done

--- cpp1/ex06/Harl.cpp
+++ cpp1/ex06/Harl.cpp
@@ -17,7 +17,7 @@ void	Harl::debug( void )
 	std::cout << "[ DEBUG ]" << std::endl;
 	std:: cout << "I love having extra bacon for my "
 		<< "7XL-double-cheese-triple-pickle-special-ketchup burger. "
-		<< "I really do!" << std::endl;
+		<< "I really do!" << std::endl << std::endl;
 }
 
 void	Harl::info( void )
@@ -25,7 +25,7 @@ void	Harl::info( void )
 	std::cout << "[ INFO ]" << std::endl;
 	std:: cout << "I cannot believe adding extra bacon costs more money. "
 		<< "You didn’t put enough bacon in my burger! "
-		<< "If you did, I wouldn’t be asking for more!" << std::endl;
+		<< "If you did, I wouldn’t be asking for more!" << std::endl << std::endl;
 }
 
 void	Harl::warning( void )
@@ -33,7 +33,7 @@ void	Harl::warning( void )
 	std::cout << "[ WARNING ]" << std::endl;
 	std:: cout << "I think I deserve to have some extra bacon for free. "
 		<< "I’ve been coming for years, whereas you "
-		<< "started working here just last month." << std::endl;
+		<< "started working here just last month." << std::endl << std::endl;
 }
 
 void	Harl::error( void )
@@ -55,9 +55,26 @@ void	Harl::complain( std::string level )
 		&Harl::error
 	};
 
-	switch ()
+	int i = 0;
+	while (i < 4)
 	{
-	
+		if (levels[i] == level)
+			break;
+		i++;
 	}
-	std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+
+	switch (i)
+	{
+		case 0:
+			(this->*funcPtr[0])();
+		case 1:
+			(this->*funcPtr[1])();
+		case 2:
+			(this->*funcPtr[2])();
+		case 3:
+			(this->*funcPtr[3])();
+			break;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
 }
--- cpp1/ex06/main.cpp
+++ cpp1/ex06/main.cpp
@@ -5,6 +5,7 @@ int	main( int ac, char **av )
 	if (ac != 2)
 	{
 		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+		return 1;
 	}
 	std::string	choice = av[1];
 
