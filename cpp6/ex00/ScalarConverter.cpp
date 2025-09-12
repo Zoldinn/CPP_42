02e86f26ef4e53fa5ca4f67924cde099daafb518
lefoffan - leon <lefoffan@student.42perpignan.fr>
2025-09-12 15:07:07 +0200

    or maybe i was not that wrong ?

--- cpp6/ex00/ScalarConverter.cpp
+++ cpp6/ex00/ScalarConverter.cpp
@@ -11,74 +11,32 @@ ScalarConverter& ScalarConverter::operator=( const ScalarConverter& other )
 
 /*============================================================================*/
 
-bool	isNumber( std::string str )
+void	convertToChar( std::string str )
 {
-	if ( str[0] != '-' || str[0] != '+' || !isdigit(str[0]) )
-		return false;
-	for ( size_t i = 1; i < str.length(); i++ )
-	{
-		if ( !isdigit(str[i]) )
-		{
-			if ( !( i < str.length() - 1 && str[i] == '.' ) )
-				return false;
-			else if ( i == str.length() - 1 && str[i] != 'f' )
-				return false;
-		}
-	}
-	return true;
+	char	val;
+
+	val
 }
 
-void*	isPseudoLiteral( std::string str )
+void	convertToInt( std::string str )
 {
-	if ( str == "-inff" || str == "+inff" || str == "nanf" )
-		return	new float( static_cast<float>( atof(str.c_str()) ) );
-	if ( str == "-inf" || str == "+inf" || str == "nan" )
-		return	new double( atof(str.c_str()) );
-	return	NULL;
+
 }
 
-void*	getType( std::string str )
+void	convertToFloat( std::string str )
 {
-	const char*	cstr = str.c_str();
 
-	if ( str.length() == 1 && isalpha(static_cast<int>( cstr[0] )) && isprint(cstr[0]) )
-		return	new char( static_cast<char>( str[0] ) ); 
-	if ( isNumber(str) )
-	{
-		if ( str.find('.') != std::string::npos )
-		{
-			if ( str.find('f') != std::string::npos )
-				return	new float( static_cast<float>( atof(cstr) ) );
-			return	new double( atof(cstr) );
-		}
-		return	new int( atoi(cstr ));
-	}
-	return	new void*( isPseudoLiteral(str) );
 }
 
-// todo : limits
+void	convertToDouble( std::string str )
+{
+
+}
+
 void	ScalarConverter::convert( std::string str )
 {
-	void*	val;
-	char*	charVal;
-	int*	intVal;
-	float*	floatVal;
-	double*	doubleVal;
-
-	val	= getType( str );
-	if (!val)
-	{
-		std::cout << "Don't match any type" << std::endl;
-		return ;
-	}
-	charVal		= static_cast<char *>(val);
-	intVal		= static_cast<int *>(val);
-	floatVal	= static_cast<float *>(val);
-	doubleVal	= static_cast<double *>(val);
-
-	std::cout	<< *charVal		<< std::endl
-				<< *intVal		<< std::endl
-				<< *floatVal	<< std::endl
-				<< *doubleVal	<< std::endl;
-	delete	val;
+	convertToChar( str );
+	convertToInt( str );
+	convertToFloat( str );
+	convertToDouble( str );
 }
