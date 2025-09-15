5659c8a274dd0e0ae5c9ce1314e376b8582707e8
lefoffan - leon <lefoffan@student.42perpignan.fr>
2025-09-15 12:01:49 +0200

    thing to mix what i did before and what i was thinking recently

--- cpp6/ex00/ScalarConverter.cpp
+++ cpp6/ex00/ScalarConverter.cpp
@@ -11,32 +11,87 @@ ScalarConverter& ScalarConverter::operator=( const ScalarConverter& other )
 
 /*============================================================================*/
 
-void	convertToChar( std::string str )
+bool	isNumber( std::string str )
 {
-	char	val;
-
-	val
+	if ( str[0] != '-' || str[0] != '+' || !isdigit(str[0]) )
+		return false;
+	for ( size_t i = 1; i < str.length(); i++ )
+	{
+		if ( !isdigit(str[i]) )
+		{
+			if ( !( i < str.length() - 1 && str[i] == '.' ) )
+				return false;
+			else if ( i == str.length() - 1 && str[i] != 'f' )
+				return false;
+		}
+	}
+	return true;
 }
 
-void	convertToInt( std::string str )
+void*	isPseudoLiteral( std::string str )
 {
-
+	if ( str == "-inff" || str == "+inff" || str == "nanf" )
+		return	new float( static_cast<float>( atof(str.c_str()) ) );
+	if ( str == "-inf" || str == "+inf" || str == "nan" )
+		return	new double( atof(str.c_str()) );
+	return	NULL;
 }
 
-void	convertToFloat( std::string str )
+void*	getType( std::string str )
 {
+	const char*	cstr = str.c_str();
 
+	if ( str.length() == 1 && isalpha(static_cast<int>( cstr[0] )) && isprint(cstr[0]) )
+		return	new char( static_cast<char>( str[0] ) ); 
+	if ( isNumber(str) )
+	{
+		if ( str.find('.') != std::string::npos )
+		{
+			if ( str.find('f') != std::string::npos )
+				return	new float( static_cast<float>( atof(cstr) ) );
+			return	new double( atof(cstr) );
+		}
+		return	new int( atoi(cstr ));
+	}
+	return	new void*( isPseudoLiteral(str) );
 }
 
-void	convertToDouble( std::string str )
+void	convertFromChar( char* val )
 {
-
+	std::cout	<< "char: " << val << std::endl
+				<< static_cast<int>( *val ) << std::endl
+				<< static_cast<float>( *val ) << std::endl
+				<< static_cast<double>( *val ) << std::endl;
 }
 
+void	convertFromInt( int* val )
+{
+	
+}
+
+// todo : limits
 void	ScalarConverter::convert( std::string str )
 {
-	convertToChar( str );
-	convertToInt( str );
-	convertToFloat( str );
-	convertToDouble( str );
+	void*	val;
+	char*	charVal;
+	int*	intVal;
+	float*	floatVal;
+	double*	doubleVal;
+
+	val	= getType( str );
+	if (!val)
+	{
+		std::cout << "Don't match any type" << std::endl;
+		return ;
+	}
+	charVal*		= convertToChar( val );
+	intVal*		= convertToInt( val );
+	floatVal*	= convertToFloat( val );
+	doubleVal*	= convertToDouble( val );
+
+	std::cout	<< *charVal		<< std::endl
+				<< *intVal		<< std::endl
+				<< *floatVal	<< std::endl
+				<< *doubleVal	<< std::endl;
+	delete	val;
 }
