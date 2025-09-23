3cca0c10cfc609fbcd19bf41503a12090267565b
lefoffan - leon <lefoffan@student.42perpignan.fr>
2025-09-23 15:26:32 +0200

    just put the 2 functions of Serializer.cpp in main to have less files

--- cpp6/ex01/main.cpp
+++ cpp6/ex01/main.cpp
@@ -1,5 +1,15 @@
 #include "Serializer.hpp"
 
+uintptr_t	Serializer::serializer( Data* ptr )
+{
+	return reinterpret_cast<uintptr_t>( ptr );
+}
+
+Data*		Serializer::deserializer( uintptr_t raw )
+{
+	return reinterpret_cast<Data*>( raw );
+}
+
 int	main( void )
 {
 	Data		a;
