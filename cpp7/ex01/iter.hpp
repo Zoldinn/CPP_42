7788358f08e0d11fe526fcad50746885c3a2b72a
lefoffan - leon <lefoffan@student.42perpignan.fr>
2025-09-30 13:41:34 +0200

    added tests to ex01 and change T arr[] to T* arr, its clearer for me

--- cpp7/ex01/iter.hpp
+++ cpp7/ex01/iter.hpp
@@ -12,15 +12,19 @@ template <typename T>
 void	minusOne( T& data ) { data -= 1; };
 
 template <typename T>
-void	iter( T arr[], const int size, void (*f)(T&) )
+void	iter( T* arr, const int size, void (*f)(T&) )
 {
+	if ( !arr || size <= 0 || !f )
+		return;
 	for ( int i = 0; i < size; i++ )
 		f( arr[i] );
 }
 
 template <typename T>
-void	iter( const T arr[], const int size, void (*f)(const T&) )
+void	iter( const T* arr, const int size, void (*f)(const T&) )
 {
+	if ( !arr || size <= 0 || !f )
+		return;
 	for ( int i = 0; i < size; i++ )
 		f( arr[i] );
 }
--- cpp7/ex01/main.cpp
+++ cpp7/ex01/main.cpp
@@ -2,16 +2,29 @@
 
 int	main( void )
 {
-	int	arr[] = { 1, 2, 3, 4, 5};
+	int	arr1[] = { 1, 2, 3, 4, 5};
 
 	std::cout << "Before" << std::endl;
-	iter( arr, 5, &print );
+	iter( arr1, 5, &print );
 	std::cout << "Plus one" << std::endl;
-	iter( arr, 5, &plusOne );
-	iter( arr, 5, &print );
+	iter( arr1, 5, &plusOne );
+	iter( arr1, 5, &print );
 	std::cout << "Minus one" << std::endl;
-	iter( arr, 5, &minusOne );
-	iter( arr, 5, &print );
+	iter( arr1, 5, &minusOne );
+	iter( arr1, 5, &print );
+
+	/*===================================*/
+	
+	char	arr2[] = { 'a', 'b', 'c', 'd', 'e' };
+
+	std::cout << "Before" << std::endl;
+	iter( arr2, 5, &print );
+	std::cout << "Plus one" << std::endl;
+	iter( arr2, 5, &plusOne );
+	iter( arr2, 5, &print );
+	std::cout << "Minus one" << std::endl;
+	iter( arr2, 5, &minusOne );
+	iter( arr2, 5, &print );
 
 	return 0;
 }
