823a205f452f9066aa656b63a9045c9069fdf19a
lefoffan - leon <lefoffan@student.42perpignan.fr>
2025-10-02 18:23:32 +0200

    start thinking how doing cpp9/ex00

--- cpp9/ex00/BitcoinExchange.hpp
+++ cpp9/ex00/BitcoinExchange.hpp
@@ -7,13 +7,18 @@
 class BitcoinExchange
 {
 	public:
-		BitcoinExchange( void );
+		BitcoinExchange( std::string inputFile );
 		BitcoinExchange( const BitcoinExchange& copy );
 		~BitcoinExchange( void );
-
 		BitcoinExchange&				operator=( const BitcoinExchange& other );
-		std::fstream					fs;
+
+		void							solver( void ) const;
 
 	private:
-		std::map<std::string, float>	_dtb;
+		BitcoinExchange( void );
+		std::fstream					_fs_tp; // file stream time/price
+		std::fstream					_fs_ti; // file stream time/input
+		std::map<std::string, float>	_dtb_tp; // database time/price
+		std::map<std::string, float>	_dtb_ti; // database time/input
+		void							_fill( std::map<std::string, float> dtb, std::fstream fs ); // to fill a dtb
 };
