5862dd8ead740c83f2937a37552db42eca912cfa
lefoffan - leon <lefoffan@student.42perpignan.fr>
2025-10-02 11:59:30 +0200

    cpp8/ex02 added tests, seem ok

--- cpp8/ex02/main.cpp
+++ cpp8/ex02/main.cpp
@@ -32,5 +32,38 @@ int	main( void )
 	}
 	std::cout << std::endl;
 
+
+	std::cout << "========== COMPARE WITH LIST (should print same)==========" << std::endl << std::endl;
+
+	std::list<int>	list;
+
+	list.push_back(5);		std::cout << "list.push_back(5)"	<< std::endl;
+	list.push_back(17);		std::cout << "list.push_back(17)"	<< std::endl << std::endl;
+
+	std::cout << "list.back() = " << list.back() << std::endl << std::endl;
+
+	list.pop_back();		std::cout << "list.pop_back()"		<< std::endl << std::endl;
+
+	std::cout << "list.size() = " << list.size() << std::endl;
+	std::cout << "list.back() = " << list.back() << std::endl << std::endl;
+
+	list.push_back(3);		std::cout << "list.push_back(3)"	<< std::endl;
+	list.push_back(5);		std::cout << "list.push_back(5)"	<< std::endl;
+	list.push_back(737);	std::cout << "list.push_back(737)"	<< std::endl;
+	list.push_back(0);		std::cout << "list.push_back(0)"	<< std::endl << std::endl;
+
+	std::cout << "list.back() = " << list.back() << std::endl << std::endl;
+
+	std::list<int>::iterator lit  = list.begin();
+	std::list<int>::iterator lite = list.end();
+	while (lit != lite)
+	{
+		std::cout << *lit;
+		if ( ++lit != lite )
+			std::cout << ", ";
+	}
+	std::cout << std::endl;
+
+
 	return 0;
 }
